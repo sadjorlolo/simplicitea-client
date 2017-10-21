db858ee948db37478db6ec1fbf9c7f46291afead
sadjorlolo <sarah.adjorlolo@libertymutual.com>
2017-10-21 19:07:29 -0400

    Add "View Review" button to handlebar
    - Add "View Review" button to handlebar instead of adding click
    event to entire div.
    - Change spacing
    - Edit events.js to reflect new hierarchy for retrieving data-id

--- assets/scripts/reviews/events.js
+++ assets/scripts/reviews/events.js
@@ -14,7 +14,7 @@ const onGetReviews = function (event) {
 
 const onGetReview = function (event) {
   event.preventDefault()
-  const selectedReviewId = $(this).attr('data-id')
+  const selectedReviewId = $(this).parent().attr('data-id')
   api.show(selectedReviewId)
     .then(ui.getReviewSuccess)
     .catch(ui.getReviewError)
@@ -23,13 +23,13 @@ const onGetReview = function (event) {
 const onDeleteReview = function (event) {
   event.preventDefault()
   console.log('ondelete event is', event)
-  const reviewForDelete = $(this).attr('id')
+  const reviewForDelete = $(this).parent().attr('data-id')
   console.log(reviewForDelete)
 }
 
 const addHandlers = function () {
-  $('.see-reviews-content').on('click', '.see-review', onGetReview)
   $('.see-reviews').on('submit', onGetReviews)
+  $('.see-reviews-content').on('click', '.view-review', onGetReview)
   $('.see-review-content').on('click', '.delete-review', onDeleteReview)
 }
 
